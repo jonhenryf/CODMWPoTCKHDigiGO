876813bb15dbd09ef7548a7233aa42e1a31f9b83
jonhenry <jonhenryf@gmail.com>
2019-11-24 17:28:18 -0800

    added items header

---
+++ items.h
@@ -0,0 +1,11 @@
+//
+// Created by mrjof on 11/24/2019.
+//
+
+#ifndef CODMWPOTCKHDIGIGO_ITEMS_H
+#define CODMWPOTCKHDIGIGO_ITEMS_H
+
+#endif //CODMWPOTCKHDIGIGO_ITEMS_H
+
+//This header will be used for all items we put into the game
+// will use the design patterns we stated on the assignment
